7cc80c8ade4b5ff64a5882470f2a9900c3286050
It's_Suarabh_Kumar <36292120+skumar860@users.noreply.github.com>
2024-03-11 13:18:23 +0530

    Add files via upload

---
+++ Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default function Player({initialName,symbol,isActive}) {
+
+    const[playerName,setPlayerName]=useState(initialName)
+    const [isEditing,setIsEditing]= useState(false)
+
+    let buttonValue="Edit"
+    let editablePlayerName=<span className="player-name">{playerName}</span>
+
+    function handleChange(event){
+      setPlayerName(event.target.value)
+    }
+    function handleEditClick(){
+        // (isEditing? setIsEditing(false) :setIsEditing(true))
+        //  or we can do below that is best practice in react to call upadting stae function on basis of older value
+        setIsEditing((editing)=> !editing)
+     }
+     if(isEditing){
+
+        buttonValue="Save"
+        editablePlayerName=<input type="text" required value={playerName} onChange={handleChange} />
+     }
+     else
+     {
+        buttonValue="Edit"
+        editablePlayerName=<span className="player-name">{playerName}</span>
+     }
+
+    return <li className={isActive ? 'active' : undefined}>
+        <span className="player">
+            {editablePlayerName}
+            <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick} >{buttonValue}</button>
+    </li>
+}
